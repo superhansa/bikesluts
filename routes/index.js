88161e0da3b50db120473f1d05fd5fe132c21e4d
Hans Fredrik Vestneshagen <superhansa@gmail.com>
2017-06-01 21:25:47 +0200

    start reading json

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+var jsonReader = require('../lib/jsonReader');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  jsonReader.read('./data/trips-2017.5.1-2017.5.31.json',function(fileContent){
+    res.render('index', { title: 'Express', jsonFile: fileContent });
+  });
+  
 });
 
 module.exports = router;
