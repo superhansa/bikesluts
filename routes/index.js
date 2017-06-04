4b0b2d76e05bca88ee51e5e5c3774217f6c2b177
Martin Sjåstad <m.sjastad@gmail.com>
2017-06-04 21:13:29 +0200

    Stream out and parse a full month worth of trip data from bysykkel. + Return a list of the months most promiscuous stations.

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,81 @@
-var express = require('express');
-var router = express.Router();
-var JSONStream = require('JSONStream');
-var jsonReader = require('../lib/jsonReader');
+var express = require('express')
+var router = express.Router()
+var JSONStream = require('JSONStream')
+var _ = require('lodash')
+
+var jsonReader = require('../lib/jsonReader')
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  jsonReader.read('./data/10trips.json', function(fileContent){
-    var parsed = JSON.parse(fileContent);
-    console.log(parsed);
+router.get('/', function (req, res, next) {
+  jsonReader.read('./data/10trips.json', function (fileContent) {
+    var parsed = JSON.parse(fileContent)
+    console.log(parsed)
 
-    var start_stations = parsed.trips.map(function (trip) {
-      return trip.start_station_id;
-    });
+    var startStations = parsed.trips.map(function (trip) {
+      return trip.start_station_id
+    })
 
-    res.render('index', { title: 'Express', jsonFile: start_stations });
-  });
-  
-});
+    res.render('index', { title: 'Express', jsonFile: startStations })
+  })
+})
 
-router.get('/stream', function (req, res, next) {
-  jsonReader.readStartStationStream('./data/trips-2017.5.1-2017.5.31.json').pipe(res);
-});
+router.get('/stream10', function (req, res, next) {
+  var occurrences = {}
+  jsonReader.readStream('./data/10trips.json')
+    .pipe(JSONStream.parse('trips.*'))
+    .on('data', function (data) {
+      var startStationId = parseInt(data.start_station_id, 10)
+      var endStationId = parseInt(data.end_station_id, 10)
+      if (typeof occurrences[startStationId] === 'undefined') {
+        occurrences[startStationId] = {count: 1}
+      } else {
+        occurrences[startStationId].count++
+      }
 
-module.exports = router;
+      if (typeof occurrences[endStationId] === 'undefined') {
+        occurrences[endStationId] = {count: 1}
+      } else {
+        occurrences[endStationId].count++
+      }
+    })
+    .on('end', function () {
+      var sorted = _.map(occurrences, function (obj, id) {
+        return {station_id: id, count: obj.count}
+      })
+      return res.render('index', {
+        title: 'Ninja Turtles',
+        stations: _.orderBy(sorted, ['count', 'station_id'], ['desc', 'asc'])
+      })
+    })
+})
+
+router.get('/full_stream_ahead', function (req, res, next) {
+  var occurrences = {}
+  jsonReader.readStream('./data/trips-2017.5.1-2017.5.31.json')
+    .pipe(JSONStream.parse('trips.*'))
+    .on('data', function (data) {
+      var startStationId = parseInt(data.start_station_id, 10)
+      var endStationId = parseInt(data.end_station_id, 10)
+      if (typeof occurrences[startStationId] === 'undefined') {
+        occurrences[startStationId] = {count: 1}
+      } else {
+        occurrences[startStationId].count++
+      }
+
+      if (typeof occurrences[endStationId] === 'undefined') {
+        occurrences[endStationId] = {count: 1}
+      } else {
+        occurrences[endStationId].count++
+      }
+    })
+    .on('end', function () {
+      var sorted = _.map(occurrences, function (obj, id) {
+        return {station_id: id, count: obj.count}
+      })
+      return res.render('index', {
+        title: 'Ninja Turtles',
+        stations: _.orderBy(sorted, ['count', 'station_id'], ['desc', 'asc'])
+      })
+    })
+})
+
+module.exports = router
