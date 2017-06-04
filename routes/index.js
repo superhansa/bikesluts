af5420a2fa9151e5a3edde14f0244435a252480a
Hans Fredrik Vestneshagen <superhansa@gmail.com>
2017-06-04 15:05:20 +0200

    Read bigass json files with streams and parse them with JSONStream

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var JSONStream = require('JSONStream');
 var jsonReader = require('../lib/jsonReader');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  jsonReader.read('./data/10trips.json',function(fileContent){
+  jsonReader.read('./data/10trips.json', function(fileContent){
     var parsed = JSON.parse(fileContent);
     console.log(parsed);
 
@@ -14,6 +15,10 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express', jsonFile: start_stations });
   });
   
+});
+
+router.get('/stream', function (req, res, next) {
+  jsonReader.readStartStationStream('./data/trips-2017.5.1-2017.5.31.json').pipe(res);
 });
 
 module.exports = router;
