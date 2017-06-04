890f5923056b7f7bfd3f0f583b52c417d2373f2b
Martin Sjåstad <m.sjastad@gmail.com>
2017-06-04 21:33:09 +0200

    Remove unused users routes and setup

--- app.js
+++ app.js
@@ -28,7 +28,6 @@ app.use(stylus.middleware(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', index)
-app.use('/users', users)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
