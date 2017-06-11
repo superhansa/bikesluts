804d830bca267fe128cf4b3dc81a86b731908dd5
Martin Sjåstad <m.sjastad@gmail.com>
2017-06-11 19:05:24 +0200

    make a graph son

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 var stylus = require('stylus')
+var hbs = require('hbs')
 
+// set up routes
 var index = require('./routes/index')
 
 var app = express()
@@ -16,6 +18,25 @@ var app = express()
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
+
+var blocks = {}
+hbs.registerHelper('extend', function (name, context) {
+  var block = blocks[name]
+  if (!block) {
+    block = blocks[name] = []
+  }
+  block.push(context.fn(this))
+})
+
+hbs.registerHelper('block', function (name) {
+  var val = (blocks[name] || []).join('\n')
+  blocks[name] = []
+  return val
+})
+
+hbs.registerHelper('json', function(data) {
+  return JSON.stringify(data)
+})
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
@@ -25,6 +46,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(stylus.middleware(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'node_modules/chart.js/dist')))
 
 app.use('/', index)
 
